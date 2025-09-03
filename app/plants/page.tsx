fca41545a863ccbc4bd68ebee23ba5d89abc4ebd
osmond <j@osmond.cc>
2025-09-02 20:40:22 -0500

    sad

--- app/plants/page.tsx
+++
@@ -1,9 +0,0 @@
-import PlantGallery from "@/components/Plants/PlantGallery";
-
-export default function PlantsPage() {
-  return (
-    <main className="p-6">
-      <PlantGallery />
-    </main>
-  );
-}
--- components/Science/GrowthExperimentLog.tsx
+++
@@ -1,8 +0,0 @@
-export default function GrowthExperimentLog() {
-  return (
-    <div className="rounded-lg bg-flora-bg dark:bg-flora-darkBg p-4 shadow-soft">
-      <h2 className="font-scientific text-lg font-bold">Growth Experiment Log</h2>
-      <p className="text-flora-water">[Log height, leaf count, photo comparisons]</p>
-    </div>
-  );
-}
